e4b455f7df8061f0b57802d8df786a4b9fcf510f
Rahul Relekar <rrelekar2-c@my.cityu.edu.hk>
2021-01-11 22:37:18 +0800

    Added more precision to Q3-REGEX

--- HelloToby/12thJanInterview/Q3-Regex/hkPhone.py
+++ HelloToby/12thJanInterview/Q3-Regex/hkPhone.py
@@ -1,11 +1,38 @@
+"""
+
+    Q3 - REGEX : Checks the validity of the Hong Kong Phone number
+    1. Start with a plus sign
+    2. Followed by 852 country code
+    3. {Optional} One whitespace character
+    4. 1 digit from a set of [2,3,5,6,7,9]
+    5. 3 digits ranging from 0-9 
+    6. {Optional} One more whitespace character
+    7. Followed by 4 more digits ranging from 0-9
+
+    Functional Example: +852 5222 0073
+
+"""
+
+# Regex python library for re.search(regex_pattern, respective_input)
 import re
 
-pattern = "^\+[852]+\s+[\d]{4}\s+[\d]{4}$"
+# regex_pattern
+pattern = "^\+(852)\s*[2,3,5,6,7,9][\d]{3}\s*[\d]{4}$"
 
+#Input from user
+print("Please input your HK phone number (+852 XXXX XXXX)")
 user_input = input()
 
-if(re.search(pattern, user_input)):
-    print("Got it right")
+while user_input != 'q':
 
-else:
-    print("nah fam")
+
+    if(re.search(pattern, user_input)):
+        print("Valid Hong Kong Phone Number")
+        print("<---------------------->\n")
+
+    else:
+        print("Invalid Hong Kong Phone Number")
+        print("<-----------XXX----------->\n")
+
+    print("Please input your HK phone number (+852 XXXX XXXX)")
+    user_input = input()
